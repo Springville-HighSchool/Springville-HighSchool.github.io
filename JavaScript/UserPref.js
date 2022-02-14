82b0c37cf5e3499a358e2bce31e896a209622c05
ZombiePelican007 <72618817+ZombiePelican007@users.noreply.github.com>
2022-02-14 16:10:35 -0700

    added dark mode

---
+++ JavaScript/UserPref.js
@@ -0,0 +1,40 @@
+function ToggleDark() {
+    if (localStorage.ModePref === "Dark") {
+        localStorage.ModePref = "Light";
+        document.location.reload(true);
+    } else if (localStorage.ModePref === "Light") {
+        localStorage.ModePref = "Dark";
+        document.location.reload(true);
+    };
+    Popup();
+};
+
+function Popup() {
+    if (localStorage.ModePref === "Dark") {
+        // add the code for dark mode here
+        document.getElementById("Popup").style.display = "none";
+        document.getElementById("ModeSwitch").className += ' Dark';
+        document.getElementById("ModeSwitch2").className += ' Dark';
+        document.getElementById("ModeSwitch3").className += ' Dark';
+        document.getElementById("DarkNav").className += ' NavFixDark';
+        document.getElementById("skewed-theme").className += ' ThemeFixDark';
+    } else if (localStorage.ModePref === "Light") {
+        document.getElementById("Popup").style.display = "none";
+    } else {
+        document.getElementById("Popup").style.display = "show"
+    };
+};
+
+function Dark() {
+    localStorage.ModePref = "Dark";
+    document.getElementById("Popup").style.display = "none";
+    Popup();
+    document.location.reload(true);
+};
+
+function Light() {
+    localStorage.ModePref = "Light";
+    document.getElementById("Popup").style.display = "none";
+    Popup();
+    document.location.reload(true);
+};
